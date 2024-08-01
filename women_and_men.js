eeb91d2f93c0dd3332fdbff3a72ca77d942009a3
Gal Cohen <galcohen211@gmail.com>
2024-08-01 23:39:23 +0300

    add timer to the close btn

--- women_and_men.js
+++ women_and_men.js
@@ -1,4 +1,22 @@
 function closeFooter() {
-    document.querySelector('.black-bar').style.display = 'none';
+    const footer = document.querySelector('.black-bar');
+    footer.style.display = 'none';
+    localStorage.setItem('footerFlag', 'true');
   }
+
+  setTimeout(() =>{
+    const closeBtn = document.querySelector('.close-btn');
+    closeBtn.style.display = 'block';
+  }, 3000);
+
+
+
+
+
+  
+
+
+
+
+
 
