6097c0050f5184cd2eab83d21d0211a2cbc55883
Yoni Katz <yehonatan.katz@biocatch.com>
2024-08-04 20:25:32 +0300

    dropdown

--- women_and_men.js
+++ women_and_men.js
@@ -7,3 +7,12 @@ setTimeout(() => {
   const closeBtn = document.querySelector('.close-btn');
   closeBtn.style.display = 'block';
   }, 3000);
+
+function openDropDown() {
+  var dropdownMenu = document.getElementById("dropdownMenu");
+  if (dropdownMenu.style.display === "none" || dropdownMenu.style.display === "") {
+    dropdownMenu.style.display = "block";
+  } else {
+    dropdownMenu.style.display = "none";
+  }
+}
