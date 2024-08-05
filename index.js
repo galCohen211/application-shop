1bc8cd130cdbb53bc0535b1ebd7d0fcae1ae3d61
Gal Cohen <galcohen211@gmail.com>
2024-08-05 14:03:04 +0300

    Changing images every 3 seconds in the homepage

--- index.js
+++ index.js
@@ -11,3 +11,19 @@ document.querySelectorAll('.men-link').forEach(function(link) {
         window.location.href = 'men.html';
     });
 });
+
+//Changing images every 3 seconds
+document.addEventListener("DOMContentLoaded", function(){
+    const image = document.getElementById("background-image");
+    const images = [
+        "images/general/homepage_background.png",
+        "images/general/homepage_background1.png",
+        "images/general/homepage_background2.png",
+        "images/general/homepage_background3.png"
+    ];
+    let currentImage = 0;
+    setInterval(()=>{
+        currentImage = (currentImage +1) % images.length;
+        image.src = images[currentImage];
+    }, 3000);
+})
