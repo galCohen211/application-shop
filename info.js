0c2b3f1ea0cf48eae1388cfe0b439db47a1bd8da
oriya_pelossof <pelossoforiya@gmail.com>
2024-08-07 01:41:59 +0300

    progress on info page and fixing other pages

--- info.js
+++ info.js
@@ -1,3 +1,9 @@
+function closeFooter() {
+    const footer = document.querySelector('.black-bar');
+    footer.style.display = 'none';
+  }
 
-
-
+  setTimeout(() => {
+    const closeBtn = document.querySelector('.close-btn');
+    closeBtn.style.display = 'block';
+    }, 3000);
