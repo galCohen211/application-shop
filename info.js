6f9ad4b0d3db3256cf145ae518319ee46d749e7f
Gal Cohen <galcohen211@gmail.com>
2024-08-11 22:15:53 +0300

    fix: hide black bar in info page

--- info.js
+++ info.js
@@ -1,13 +1,3 @@
-function closeFooter() {
-    const footer = document.querySelector('.black-bar');
-    footer.style.display = 'none';
-  }
-
-  setTimeout(() => {
-    const closeBtn = document.querySelector('.close-btn');
-    closeBtn.style.display = 'block';
-    }, 3000);
-
 //Function to show the popup
 function showPopup() {
     const popup = document.getElementById('popup');
