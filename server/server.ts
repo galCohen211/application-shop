a46e1959bcc53f2984aeca24115b418c21f9f51c
Or Shohat <milanezi45@gmail.com>
2024-08-15 19:24:15 +0300

    add basic server features

--- server/server.ts
+++ server/server.ts
@@ -6,15 +6,29 @@ import { users } from './routers/userRouter.ts'
 import { carts } from './routers/cartRouter.ts';
 import { orders } from './routers/orderRouter.ts';
 import path from 'path';
-import expressJwt from 'express-jwt';
+import { expressjwt } from "express-jwt";
+
+const PORT = 4000;
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-app.use(expressJwt({ secret: SECRET }).unless({ path: ['ALLPATHSHERE'] }));
+app.use(
+    expressjwt({
+        secret: SECRET,
+        algorithms: ["HS256"],
+    }).unless({ path: ["/token"] })
+);
 
 app.use('/users', users);
 app.use('/products', products);
 app.use('/carts', carts);
 app.use('/orders', orders);
+
+
+app.listen(PORT, () => console.log(`Server is up at ${PORT}`));
+process.on('SIGINT', function () {  //sigint its event it will happen when fire the event
+    console.log('closing mongo connection');
+    closeDb();
+})
