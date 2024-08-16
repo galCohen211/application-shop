1d0068f037b95277d4c436ca0a725f9750f25c90
Or Shohat <milanezi45@gmail.com>
2024-08-16 12:52:05 +0300

    modify user schema

--- server/schemas/userSchema.ts
+++ server/schemas/userSchema.ts
@@ -4,7 +4,6 @@ export const userSchema = Joi.object({
     firstName: Joi.string().required(),
     lastName: Joi.string().required(),
     userName: Joi.string().required(),
-    id: Joi.number().required().min(9),
     password: Joi.string().required(),
     city: Joi.string().required(),
     street: Joi.string().required(),
