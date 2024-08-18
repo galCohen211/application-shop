cd9141dc679c509c1cc56cca4dc88236cc6d37d0
Gal Cohen <galcohen211@gmail.com>
2024-08-18 21:35:50 +0300

    delete function

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -4,10 +4,80 @@ class UserController {
 
     // login
     // register
+    async registerUser (req, res) {
+        console.log("registerUser controller");
+        
+        try {
+            const {
+                firstName, 
+                lastName, 
+                email, 
+                password, 
+                city, 
+                street, 
+                gender,
+                birthDate, 
+                role
+            } = req.body;
+
+            const existUser = await User.find({ email}, function (err) {
+                if (err) {
+                    console.error(err);
+                }
+            });
+
+            if (existUser) {
+                return res.status(400).json({message: 'User already registerd'});
+            }
+
+            await Tank.create({
+                firstName, 
+                lastName, 
+                email, 
+                password, 
+                city, 
+                street, 
+                gender,
+                birthDate, 
+                role
+            }, function (err) {
+                if (err) throw err;
+            });
+            
+            return res.status(200).json({message: 'User registered successfuly'});
+
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({message: 'server error'});
+        }
+    }
+
     // delete user
+    async deleteUser (req, res){
+        try {
+            const {userId} = req.parmas;
+
+            const existUser = await User.findById(userId);
+
+            if(!existUser) {
+                return res.status(400).json({message: 'User not found'});
+            }
+
+            await User.deleteOne({_id: userId}, function (err) {
+                if (err) throw err;
+            });
+            
+            return res.status(200).json({message: 'Delete success'});
+
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({message: 'server error'});
+        }
+    }
+
     // update user
-    
 
 }
 
 module.exports = UserController;
+
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,6 +1,10 @@
 const express = require("express");
-
+const UserController = require("../Controllers/UserController");
+const userController = new UserController();
 const router = express.Router();
+
+router.post('/', userController.registerUser.bind(userController));
+router.delete('/:userId' , userController.deleteUser.bind(userController));
 
 
 module.exports = router;
