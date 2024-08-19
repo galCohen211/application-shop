e8fdbd2466ee2405ec8fa350c34c8a0c0cc6567b
Yoni Katz <yehonatan.katz@biocatch.com>
2024-08-19 11:08:07 +0300

    get users init

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -1,12 +1,14 @@
 const User = require("../models/user");
-
+const { validationResult } = require('express-validator');
 class UserController {
 
-    // login
-    // register
-    // delete user
-    // update user
-    
+    static async getUserById(req, res) {
+        let userId = req.body._id
+        const validationErrors = validationResult(req);
+        if (!validationErrors.isEmpty) { // maybe omit !
+            return res.status(400).json({ errors: validationErrors.array() })
+        }
+    }
 
 }
 
