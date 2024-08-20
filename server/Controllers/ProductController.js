a88e74d9265440ea54030348c926f5a09cde5472
oriya_pelossof <pelossoforiya@gmail.com>
2024-08-20 10:09:34 +0300

    Logic of adding a new product

--- server/Controllers/ProductController.js
+++ server/Controllers/ProductController.js
@@ -1,7 +1,64 @@
-const Product = require("../models/product");
+const product = require("../models/product");
+const mongoose = require('mongoose');
 
 class ProductController {
 
+
+//Add Product
+static async addProduct(req,res){
+    let newProduct = new Product({
+        name: req.body.name,
+        category: req.body.category,
+        price: req.body.price,
+        brand: req.body.brand,
+        size: req.body.size,
+        color: req.body.color,
+        inStock: req.body.inStock,
+        imagePath: req.body.imagePath
+    })
+
+    const validationErrors = validationResult(req);
+    if (!validationErrors.isEmpty()) {
+        return res.status(400).json({ errors: validationErrors.array() })
+    }
+
+    newProduct.save();
+    res.status(200).send({
+        name: user.name,
+        category: user.category,
+        price: user.price,
+        brand: user.brand,
+        size: user.size,
+        color: user.color,
+        inStock: user.inStock,
+        imagePath: user.imagePath,
+    });
+
 }
+
+
+
+
+
+//Delete Product
+// static async deleteProduct(req,res){
+//     const productId=req.params.id;
+//     if (!mongoose.isValidObjectId(productId)) {
+//         return res.status(400).send('Invalid Product');
+//     }
+//     Product.findByIdAndDelete(productId).then(product => {
+//         if (product) {
+//             return res.status(200).json({ success: true, message: "The product is deleted" });
+//         }
+//         else {
+//             return res.status(404).json({ success: false, message: "Product is not found" });
+//         }
+//     }).catch(err => {
+//         return res.status(500).json({ message: "server error", error: err });
+//     });
+
+// }
+}
+
 
 module.exports = ProductController;
--- server/routers/productRouter.js
+++ server/routers/productRouter.js
@@ -1,8 +1,27 @@
 const express = require("express");
+const { check } = require("express-validator")
+const ProductController = require("../Controllers/ProductController");
 
 
 const router = express.Router();
 
 
+
+//Add product
+router.post(
+    "/products",
+    check("name").notEmpty(),
+    check("category").notEmpty(),
+    check("price").notEmpty(),
+    check("brand").notEmpty(),
+    check("size").notEmpty(),
+    check("color").notEmpty(),
+    check("inStock").notEmpty(),
+    check("imagePath").notEmpty(),
+    ProductController.addProduct
+);
+
+// Delete product
+router.delete("/:id", ProductController.deleteProduct);
 
 module.exports = router;
