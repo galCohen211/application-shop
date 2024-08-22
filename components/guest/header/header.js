c5a2a18fba4704f76aa894d9ac76b783fe61e807
Yoni Katz <yehonatan.katz@biocatch.com>
2024-08-22 10:51:38 +0300

    guest header

---
+++ components/guest/header/header.js
@@ -0,0 +1,7 @@
+// Apply header.html file into the pages
+fetch('header.html')
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById('header').innerHTML = data;
+    })
+    .catch(error => console.error('Error fetching header:', error));
