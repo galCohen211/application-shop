474c7514ba66ce50656882128a7bbc095abc5bfb
oriya_pelossof <pelossoforiya@gmail.com>
2024-10-03 23:16:20 +0300

    user admin edit,delete,add,view works

--- components/admin/users/users.js
+++ components/admin/users/users.js
@@ -1,13 +1,13 @@
 // Table view and pagination
 function tableView() {
-    const rowsPerPage = 4; // Number of rows per page
+    const rowsPerPage = 3; // Number of rows per page
     const rows = $('#main-table tbody tr');
     const rowsCount = rows.length;
     const totalPages = Math.ceil(rowsCount / rowsPerPage); // How many divisions of pages we need
 
     $('#pagination').empty(); // Clear previous pagination
 
-    const pagination = $('<ul class="pagination"></ul>'); // Create the pagination element
+    const pagination = $('<ul class="pagination"></ul>'); // Cfreate the pagination element
 
     // Function to display rows for the given page number
     function displayRows(pageNumber) {
@@ -39,19 +39,26 @@ function tableView() {
     pagination.find('li:first').addClass('active'); // Mark the first page as active
 }
 
-// // Fill popup with user details
-// function fillPopup(user) {
-//     $('#id')[0].value = user._id;
-//     $('#fistName')[0].value = user.firstName;
-//     $('#lastName')[0].value = user.lastName;
-//     $('#email')[0].value = user.email;
-//     $('#city')[0].value = user.city;
-//     $('#street')[0].value = user.street;
-//     $('#gender')[0].value = user.gender;
-//     $('#birthDate')[0].value = user.birthDate;
-// }
-// TODO: to remove search function from users.js
-// Get all usersW
+// Fill popup with user details
+function fillPopup(user) {
+    $('#user_id')[0].value = user._id;
+    $('#firstName')[0].value = user.firstName;
+    $('#firstName')[0].readOnly = true;
+    $('#lastName')[0].value = user.lastName;
+    $('#lastName')[0].readOnly = true;
+    $('#email')[0].value = user.email;
+    $('#city')[0].value = user.city;
+    $('#street')[0].value = user.street;
+    $('#gender')[0].value = user.gender;
+    $('#gender')[0].disabled = true;
+    date = new Date(user.birthDate);
+    $('#birthDate')[0].value = date.toISOString().slice(0, 10);
+    $('#birthDate')[0].readOnly = true;
+    document.querySelector('[for="password"]').hidden = true;
+    $("#password")[0].hidden = true;
+
+}
+// Get all users
 async function getAllUserTable() {
     const accessToken = localStorage.getItem('accessToken');
     const result = await $.ajax({
@@ -77,6 +84,10 @@ async function getAllUserTable() {
                             <td>${user.street}</td>
                             <td>${user.gender}</td>
                             <td>${date.toISOString().slice(0, 10)}</td>
+                            <td>
+                                <button class="btn edit-btn"><i class="bi bi-pencil"></i></button>
+                                <button class="btn delete-btn"><i class="bi bi-trash"></i></button>
+                            </td>
                         </tr>
                     `;
 
@@ -92,28 +103,28 @@ async function getAllUserTable() {
     return result;
 }
 
-// // Open the popup when the plus button is clicked
-// const plusButton = document.getElementById('plusButton');
-// const closePopupBtn = document.getElementById('closePopup');
+// Open the popup when the plus button is clicked
+const plusButton = document.getElementById('plusButton');
+const closePopupBtn = document.getElementById('closePopup');
 
-// plusButton.addEventListener('click', function () {
-//     popupForm.style.display = 'flex'; // Use flexbox to center the popup
-//     $('#popup-header').text("Add New user");
-//     $('#userForm')[0].reset(); // Reset all form fields
-// });
+plusButton.addEventListener('click', function () {
+    popupForm.style.display = 'flex'; // Use flexbox to center the popup
+    $('#popup-header').text("Add New user");
+    $('#userForm')[0].reset(); // Reset all form fields
+});
 
 
-// // Close the popup when the close button (X) is clicked
-// closePopupBtn.addEventListener('click', function () {
-//     popupForm.style.display = 'none';
-// });
+// Close the popup when the close button (X) is clicked
+closePopupBtn.addEventListener('click', function () {
+    popupForm.style.display = 'none';
+});
 
-// // Close the popup when clicking outside the popup content
-// window.addEventListener('click', function (event) {
-//     if (event.target === popupForm) {
-//         popupForm.style.display = 'none';
-//     }
-// });
+// Close the popup when clicking outside the popup content
+window.addEventListener('click', function (event) {
+    if (event.target === popupForm) {
+        popupForm.style.display = 'none';
+    }
+});
 
 function refreshTable() {
     // Fetch all users and reinitialize table view and pagination
@@ -123,187 +134,138 @@ function refreshTable() {
 }
 
 // // Add or update a user
-// function addOrUpdateuser(accessToken) {
-//     $('#userForm').on('submit', function (event) {
-//         event.preventDefault(); // Prevent default form submission
+function addOrUpdateuser(accessToken) {
+    $('#userForm').on('submit', async function (event) {
+        event.preventDefault(); // Prevent default form submission
 
-//         // Create FormData object and append form fields
-//         const formData = new FormData();
-//         formData.append('firstName', $('#firstName').val());
-//         formData.append('lastName', $('#lastName').val());
-//         formData.append('email', $('#email').val());
-//         formData.append('city', $('#brand').val());
-//         formData.append('street', $('#street').val());
-//         formData.append('gender', $('#gender').val());
-//         formData.append('birthDate', $('#birthDate').val());
-//         const accessToken = localStorage.getItem('accessToken');
-//         const headerText = $('#popup-header').text();
-//         if (headerText.includes("Update")) {
-//             const userId = $('#id').val();
-//             $.ajax({
-//                 url: `http://localhost:4000/users/${userId}`,
-//                 type: 'PUT',
-//                 contentType: false,
-//                 processData: false,
-//                 headers: {
-//                     'Authorization': `Bearer ${accessToken}`
-//                 },
-//                 data: formData,
-//                 success: function () {
-//                     $('#userForm')[0].reset();
-//                     $('#popupForm').hide();
-//                     refreshTable();
-//                 },
-//                 error: function (error) {
-//                     console.error('Error updating user:', error);
-//                     alert('An error occurred while updating the user.');
-//                 }
-//             });
-//         } else {
-//             $.ajax({
-//                 url: 'http://localhost:4000/users',
-//                 type: 'POST',
-//                 contentType: false,
-//                 processData: false,
-//                 headers: {
-//                     'Authorization': `Bearer ${accessToken}`
-//                 },
-//                 data: formData,
-//                 success: function (result) {
-//                     const tableBody = $('#main-table tbody');
-//                     const newRow = `
-//                         <tr>
-//                             <td>${result.user.firstName}</td>
-//                             <td>${result.user.lastName}</td>
-//                             <td>$${result.user.email}</td>
-//                             <td>${result.user.city}</td>
-//                             <td>${result.user.street}</td>
-//                             <td>${result.user.gender}</td>
-//                             <td>${result.user.birthDate}</td>
-//                         </tr>
-//                     `;
-//                     tableBody.append(newRow);
-//                     $('#userForm')[0].reset();
-//                     $('#popupForm').hide();
-//                 },
-//                 error: function (error) {
-//                     console.error('Error adding user:', error);
-//                     alert('An error occurred while adding the user.');
-//                 }
-//             });
-//         }
-//     });
-// }
-
-// // Delete a user
-// function deleteuser(accessToken) {
-//     $('#main-table').on('click', '.delete-btn', function () {
-//         const row = $(this).closest('tr');
-//         const userId = row.find('td:first').data('id');
-
-//         if (confirm("Are you sure you want to delete this user?")) {
-//             $.ajax({
-//                 url: `http://localhost:4000/users/${userId}`,
-//                 type: 'DELETE',
-//                 headers: {
-//                     'Authorization': `Bearer ${accessToken}`
-//                 },
-//                 success: function () {
-//                     row.remove(); // Remove the row from the table
-//                     alert('user deleted successfully.');
-//                 },
-//                 error: function (error) {
-//                     console.error('Error deleting user:', error);
-//                     alert('An error occurred while deleting the user.');
-//                 }
-//             });
-//         }
-//     });
-// }
-
-// // Edit a user
-// function edituser(accessToken) {
-//     $('#main-table').on('click', '.edit-btn', function () {
-//         const row = $(this).closest('tr');
-//         const userId = row.find('td:first').data('id');
-
-//         $.ajax({
-//             url: `http://localhost:4000/users/${userId}`,
-//             type: 'GET',
-//             headers: {
-//                 'Authorization': `Bearer ${accessToken}`
-//             },
-//             success: function (result) {
-//                 popupForm.style.display = 'flex';
-//                 $('#popup-header').text("Update user");
-//                 fillPopup(result.user);
-//             },
-//             error: function (error) {
-//                 console.error('Error fetching user details:', error);
-//                 alert('An error occurred while fetching user details.');
-//             }
-//         });
-//     });
-// }
-
-// Search users
-async function searchusers(accessToken) {
-    const searchQuery = $('.search-box').val().trim();
-
-    // If search box is cleared, fetch all users again
-    if (searchQuery === '') {
-        await getAllUserTable();
-        tableView();
-        return;
+        // Create FormData object and append form fields
+        firstName = $('#firstName').val();
+        lastName = $('#lastName').val();
+        email = $('#email').val();
+        city = $('#city').val();
+        password = $('#password').val();
+        street = $('#street').val();
+        gender = $('#gender').val();
+        birthDate = $('#birthDate').val();
+        const userData = {
+            firstName,
+            lastName,
+            email,
+            password,
+            city,
+            street,
+            gender,
+            birthDate,
+        };
+    
+        const accessToken = localStorage.getItem('accessToken');
+        const headerText = $('#popup-header').text();
+        if (headerText.includes("Update")) {
+            const userId = $('#user_id').val();
+            const response = await fetch(`http://localhost:4000/users/${userId}`, {
+                method: "PUT",
+                headers: {
+                  "Content-Type": "application/json",
+                  'Authorization': `Bearer ${accessToken}`
+                },
+                body: JSON.stringify(userData),
+              });
+              const responseText = await response.text();
+        
+              console.log(response);
+              console.log(responseText);
+        
+              if (response.ok) {
+                $('#userForm')[0].reset();
+                $('#popupForm').hide();
+                refreshTable();
+              } else {
+                    console.error('Error updating user:', error);
+                    alert('An error occurred while updating the user.');
+                }
+        } else {
+            
+              const response = await fetch("http://localhost:4000/users/signup", {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(userData),
+              });
+              const responseText = await response.text();
+        
+              console.log(response);
+              console.log(responseText);
+        
+              if (response.ok) {
+                $('#userForm')[0].reset();
+                $('#popupForm').hide();
+                refreshTable();
+              } else {
+           
+                const errorMsg = JSON.parse(responseText)[0];
+                if (errorMsg.email === "unavailable") {
+                document.getElementById("form-submit-error-message").innerText =
+                    "User already exists";
+                }
+            
+                console.log("Register failed");
+                console.error(error);
+              }
+            }
+        });
     }
 
-    const result = await $.ajax({
-        url: `http://localhost:4000/users/search?name=${encodeURIComponent(searchQuery)}`,
-        type: 'GET',
-        headers: {
-            'Authorization': `Bearer ${accessToken}`
-        },
-        success: function (response) {
-            const tableBody = $("#main-table tbody");
-            tableBody.empty(); // Clear previous rows
+// Delete a user
+function deleteuser(accessToken) {
+    $('#main-table').on('click', '.delete-btn', function () {
+        const row = $(this).closest('tr');
+        const userId = row.find('td:first').data('id');
 
-            if (response.length > 0) {
-                birth = new Date(user.birthDate.toISOString().slice(0, 10));
-                response.forEach(user => {
-                    const userRow = `
-                        <tr>
-                            <td data-id="${user._id}">${user.firstName}</td>
-                            <td>${user.lastName}</td>
-                            <td>$${user.email}</td>
-                            <td>${user.city}</td>
-                            <td>${user.street}</td>
-                            <td>${user.gender}</td>
-                            <td>${birth}</td>
-                            <td>
-                                <button class="btn edit-btn"><i class="bi bi-pencil"></i></button>
-                                <button class="btn delete-btn"><i class="bi bi-trash"></i></button>
-                            </td>
-                        </tr>
-                    `;
-                    tableBody.append(userRow);
-                });
-            } else {
-                // Show empty table
-                tableBody.append('<tr><td colspan="10">No users found</td></tr>');
-            }
-
-            // Reinitialize pagination after search results
-            tableView();
-        },
-        error: function (error) {
-            console.error('Error fetching search results:', error);
-            // No need to throw an error; simply show an empty table with headers
-            $("#main-table tbody").empty();
-            tableView(); // Clear pagination if no results
+        if (confirm("Are you sure you want to delete this user?")) {
+            $.ajax({
+                url: `http://localhost:4000/users/${userId}`,
+                type: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`
+                },
+                success: function () {
+                    row.remove(); // Remove the row from the table
+                    alert('user deleted successfully.');
+                    refreshTable();
+                },
+                error: function (error) {
+                    console.error('Error deleting user:', error);
+                    alert('An error occurred while deleting the user.');
+                }
+            });
         }
     });
+}
 
-    return result;
+// Edit a user
+function edituser(accessToken) {
+    $('#main-table').on('click', '.edit-btn', function () {
+        const row = $(this).closest('tr');
+        const userId = row.find('td:first').data('id');
+
+        $.ajax({
+            url: `http://localhost:4000/users/${userId}`,
+            type: 'GET',
+            headers: {
+                'Authorization': `Bearer ${accessToken}`
+            },
+            success: function (result) {
+                popupForm.style.display = 'flex';
+                $('#popup-header').text("Update user");
+                fillPopup(result.user);
+            },
+            error: function (error) {
+                console.error('Error fetching user details:', error);
+                alert('An error occurred while fetching user details.');
+            }
+        });
+    });
 }
 
 $(document).ready(async function () {
@@ -312,7 +274,7 @@ $(document).ready(async function () {
     tableView();
 
     // Event listeners
-    //addOrUpdateuser(accessToken);
-    //deleteuser(accessToken);
-    //edituser(accessToken);
+    addOrUpdateuser(accessToken);
+    deleteuser(accessToken);
+    edituser(accessToken);
 });
